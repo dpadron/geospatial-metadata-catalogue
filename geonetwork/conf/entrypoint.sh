e55e5752fc1a1109869c27719d8723944514576e
elasticlabs <patrice.mauny@gmail.com>
2020-06-04 01:20:51 +0200

    geonetwork 3.10 + elk, integrated

--- geonetwork/conf/entrypoint.sh
+++ geonetwork/conf/entrypoint.sh
@@ -9,4 +9,12 @@ if [ "$1" = 'catalina.sh' ]; then
 	export CATALINA_OPTS="$CATALINA_OPTS -Dgeonetwork.dir=$DATA_DIR"
 fi
 
+#
+# Setup Elasticsearch & Kibana from docker-compose file environment
+[ "$ES_URL" != "" ] && sed -i "s#es.host=localhost#es.host=${ES_HOST}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties
+[ "$ES_USERNAME" != "" ] && sed -i "s#es.username=#es.username=${ES_USERNAME}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties
+[ "$ES_PASSWORD" != "" ] && sed -i "s#es.password=#es.password=${ES_PASSWORD}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties
+# Kibana
+[ "$KB_URL" != "" ] && sed -i "s#kb.url=#kb.url=${KB_URL}#" $CATALINA_HOME/webapps/geonetwork/WEB-INF/config.properties
+
 exec "$@"
