baba2c2fb0d12ce2671cedf0ce946fad32e79cb3
elasticlabs <patrice.mauny@gmail.com>
2020-06-04 15:15:59 +0200

    Added Kibana & ES... Again :)

--- geonetwork/conf/entrypoint.sh
+++ geonetwork/conf/entrypoint.sh
@@ -11,10 +11,10 @@ fi
 
 #
 # Setup Elasticsearch & Kibana from docker-compose file environment
-#[ "$ES_URL" != "" ] && sed -i "s#es.host=localhost#es.host=${ES_HOST}#" $CATALINA_HOME/webapps/ROOT/WEB-INF/data/config.properties
-#[ "$ES_USERNAME" != "" ] && sed -i "s#es.username=#es.username=${ES_USERNAME}#" $CATALINA_HOME/webapps/ROOT/WEB-INF/data/config.properties
-#[ "$ES_PASSWORD" != "" ] && sed -i "s#es.password=#es.password=${ES_PASSWORD}#" $CATALINA_HOME/webapps/ROOT/WEB-INF/data/config.properties
+[ "$ES_URL" != "" ] && sed -i "s#es.host=localhost#es.host=${ES_HOST}#" $CATALINA_HOME/webapps/ROOT/WEB-INF/data/config.properties
+[ "$ES_USERNAME" != "" ] && sed -i "s#es.username=#es.username=${ES_USERNAME}#" $CATALINA_HOME/webapps/ROOT/WEB-INF/data/config.properties
+[ "$ES_PASSWORD" != "" ] && sed -i "s#es.password=#es.password=${ES_PASSWORD}#" $CATALINA_HOME/webapps/ROOT/WEB-INF/data/config.properties
 # Kibana
-#[ "$KB_URL" != "" ] && sed -i "s#kb.url=#kb.url=${KB_URL}#" $CATALINA_HOME/webapps/ROOT/WEB-INF/data/config.properties
+[ "$KB_URL" != "" ] && sed -i "s#kb.url=#kb.url=${KB_URL}#" $CATALINA_HOME/webapps/ROOT/WEB-INF/data/config.properties
 
 exec "$@"
