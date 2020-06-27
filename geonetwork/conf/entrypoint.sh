c3245f3e2bc5afb9d4f4cf145d18b59b91c1037e
elasticlabs <patrice.mauny@gmail.com>
2020-06-27 22:44:46 +0200

    geonetwork entrypoint fix

--- geonetwork/conf/entrypoint.sh
+++ geonetwork/conf/entrypoint.sh
@@ -16,16 +16,16 @@ if [ "$ES_URL" != "" ]; then
 	sed -i "s#es.url=*#es.url=${ES_HOST}#" ${INSTALL_DIR}/WEB-INF/config.properties
 	# Elasticsearch indexes setup 
 	# TODO : full index creation automation when building geonetwork or elasticsearch directly
-	sed -i "s#es.index.features=*=*#es.index.features=gn-features#" ${INSTALL_DIR}/WEB-INF/config.properties
-	sed -i "s#es.index.records=*#es.index.records=gn-records#" ${INSTALL_DIR}/WEB-INF/config.properties
-	sed -i "s#es.index.searchlogs=*#es.index.searchlogs=gn-searchlogs#" ${INSTALL_DIR}/WEB-INF/config.properties
+	sed -i "/es.index.features=/c\es.index.features=gn-features" ${INSTALL_DIR}/WEB-INF/config.properties
+	sed -i "/es.index.records=/c\es.index.records=gn-records" ${INSTALL_DIR}/WEB-INF/config.properties
+	sed -i "/es.index.searchlogs=/c\es.index.searchlogs=gn-searchlogs" ${INSTALL_DIR}/WEB-INF/config.properties
 	
 	# User and password, if set in elasticsearch & compose file
-	[ "$ES_USERNAME" != "" ] && sed -i "s#es.username=#es.username=${ES_USERNAME}#" ${INSTALL_DIR}/WEB-INF/config.properties
-	[ "$ES_PASSWORD" != "" ] && sed -i "s#es.password=#es.password=${ES_PASSWORD}#" ${INSTALL_DIR}/WEB-INF/config.properties
+	[ "$ES_USERNAME" != "" ] && sed -i "/es.username=/c\es.username=${ES_USERNAME}" ${INSTALL_DIR}/WEB-INF/config.properties
+	[ "$ES_PASSWORD" != "" ] && sed -i "/es.password=/c\es.password=${ES_PASSWORD}" ${INSTALL_DIR}/WEB-INF/config.properties
 fi
 
 # Kibana
-[ "$KB_URL" != "" ] && sed -i "s#kb.url=*#kb.url=${KB_URL}#" ${INSTALL_DIR}/WEB-INF/config.properties
+[ "$KB_URL" != "" ] && sed -i "/kb.url=*/c\kb.url=${KB_URL}" ${INSTALL_DIR}/WEB-INF/config.properties
 
 exec "$@"
